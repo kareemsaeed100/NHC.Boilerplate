0b983978111508cdf34649e77f8b51a7d6017adc
Kareem <k.ahmed.c@nhci.sa>
2025-07-20 16:55:37 +0300

    Hangfire

---
+++ src/NHC.Boilerplate.BackgroundJob/HangfireJobRegistrar.cs
@@ -0,0 +1,22 @@
+﻿using Hangfire;
+using NHC.Boilerplate.BackgroundJob.Jobs;
+namespace NHC.Boilerplate.BackgroundJob
+{
+    public static class HangfireJobRegistrar
+    {
+        public static void Register()
+        {
+            Hangfire.BackgroundJob.Schedule<SampleJobWrapper>(
+                job => job.Execute(),
+                TimeSpan.FromMinutes(1)
+            );
+
+            RecurringJob.AddOrUpdate<SampleJobWrapper>(
+                "SampleJobRecurring",
+                job => job.Execute(),
+                Cron.Minutely
+            );
+        }
+    }
+
+}
---
+++ src/NHC.Boilerplate.BackgroundJob/Jobs/SampleJobWrapper.cs
@@ -0,0 +1,19 @@
+﻿using Abp.Dependency;
+using NHC.Boilerplate.SampleJob;
+
+namespace NHC.Boilerplate.BackgroundJob.Jobs;
+
+public class SampleJobWrapper : ITransientDependency
+{
+    private readonly ISampleJobAppService _sampleJobAppService;
+
+    public SampleJobWrapper(ISampleJobAppService sampleJobAppService)
+    {
+        _sampleJobAppService = sampleJobAppService;
+    }
+
+    public void Execute()
+    {
+        _sampleJobAppService.DoWork();
+    }
+}
